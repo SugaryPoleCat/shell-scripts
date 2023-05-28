a02dbf7aea8da9fc49a5f7b15c4380d3a30c2b8a
SugaryPoleCat <sugarypolecat@gmail.com>
2023-05-29 00:50:18 +0200

    asdsad

--- system-initialize.sh
+++ system-initialize.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d "$LOG_FOLDER" ]; then
+	mkdir "$LOG_FOLDER"
+fi
+
 # Get the log_file
 log_file="$LOG_FOLDER/apt-update.log"
 
@@ -13,15 +17,15 @@ sudo bash $SCRIPT_FOLDER/apt-update.sh
 sudo bash $SCRIPT_FOLDER/apt-packages.sh
 
 # git repos
-if[ ! -d "$PROGRAMMING_FOLDER" ]; then
+if [ ! -d "$PROGRAMMING_FOLDER" ]; then
 	mkdir "$PROGRAMMING_FOLDER"
 fi
 
-if[ ! -d "$DISCORD_FOLDER" ]; then
+if [ ! -d "$DISCORD_FOLDER" ]; then
 	mkdir "$DISCORD_FOLDER"
 fi
 
-if[ ! -d "$WEBSITES_FOLDER" ]; then
+if [ ! -d "$WEBSITES_FOLDER" ]; then
 	mkdir "$WEBSITES_FOLDER"
 fi
 
@@ -32,6 +36,7 @@ cd $WEBSITES_FOLDER
 git clone $GIT_HUB/$WEBSITE_REPO_TEST
 
 cd $SCRIPT_FOLDER
-sudo bash $SCRIPT_FOLDER/discord-bot-run.sh
+# not sure about those things being necessary
+# sudo bash $SCRIPT_FOLDER/discord-bot-run.sh
 
 # Create the thing for the website stuff
