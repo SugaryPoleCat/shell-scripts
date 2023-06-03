c7a23ed36b67187308fa3cfc6dacae647174d92d
SugaryPoleCat <sugarypolecat@gmail.com>
2023-06-04 00:28:38 +0200

    mewo

--- website-test-run.sh
+++ website-test-run.sh
@@ -5,14 +5,14 @@ if ! command -v tmux &> /dev/null; then
     exit 1
 fi
 
-# cd $WEBSITES_FOLDER/$WEBSITE_REPO_TEST
-cd $WEBSITES_FOLDER/test
+cd $WEBSITES_FOLDER/$WEBSITE_REPO_TEST
+# cd $WEBSITES_FOLDER/test
 npm i
-# tsc --build
-# cd vue
-# npm i
-# npm run build
-# cd ../
-# tmux new-session -d -s $SESSION_NAME_WEBSITE "nodemon dist/server.js --watch dist/"
-tmux new-session -d -s webtest "nodemon test.js --watch ."
+tsc --build
+cd vue
+npm i
+npm run build
+cd ../
+tmux new-session -d -s $SESSION_NAME_WEBSITE "nodemon dist/server.js --watch dist/"
+# tmux new-session -d -s webtest "nodemon test.js --watch ."
 cd $SCRIPT_FOLDER
